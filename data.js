cd4daf7242f4a9ce58da107edff9bee27a83947f
JorenGoessens <joregoes@student.arteveldehs.be>
2020-03-21 18:41:45 +0100

    Update data.js
    added xd link

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/0504abb1-9843-4db2-63c6-1aaed99efa4e-7335/",
                 "target": "_blank"
             }
         ]
